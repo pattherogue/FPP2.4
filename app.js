36d14567aca990c266e742e36126a0935ddcac8d
Paolo Gomez <peweegardner248@gmail.com>
2024-01-11 10:43:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -22,3 +22,38 @@ if (emblemClue1 === "Eagle") {
 } else {
     locationStart = "Villa";
 }
+
+/* Use boolean logic to decipher the second clue.
+
+- If `emblemClue2` is "Laurel" AND the first location is "Forum", append " of Augustus" to the location.
+- If `emblemClue2` is "Grapes" OR the first location is "Villa", append " of Pompey" to the location.
+
+*/
+
+if (emblemClue2 === "Laurel" && locationStart === "Forum") {
+    locationStart += " of Augustus"
+} else if ( emblemClue2 === "Grapes" || locationStart === "Villa") {
+    locationStart += " of Pompey"
+}
+
+/* - Depending on the value of `emblemClue3`, append a direction to the location.
+    - 7 is "North"
+    - 3 is "South"
+    - 9 is "East"
+    - 4 is "West"
+*/
+
+switch (emblemClue3) {
+    case 7:
+       locationStart += " North";
+       break;
+    case 3:
+        locationStart += " South";
+        break;
+    case 9: 
+        locationStart += " East";
+        break;
+    case 4:
+        locationStart += " West";
+        break;
+}
